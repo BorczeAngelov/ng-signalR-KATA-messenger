c3658d7499772271a5a90521f8453e548c200998
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-11-20 20:18:23 +0100

    create and register MessengerHub

---
+++ server-signalR/Hubs/MessengerHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace server_signalR.Hubs
+{
+    public class MessengerHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Caller.SendAsync("onConnected", "Testing: you are connected.");
+            Clients.Others.SendAsync("onConnected", "Testing: another client has connected.");
+            return base.OnConnectedAsync();
+        }
+    }
+}
--- server-signalR/Startup.cs
+++ server-signalR/Startup.cs
@@ -16,6 +16,16 @@ namespace server_signalR
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+
+            services.AddSignalR();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +36,14 @@ namespace server_signalR
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                endpoints.MapHub<Hubs.MessengerHub>("/messenger");
             });
         }
     }
